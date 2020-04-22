89292453909cc4ca8935850dff5b6f11d9f21a92
Aaron Patterson <aaron.patterson@gmail.com>
2020-04-22 11:43:36 -0700

    adding temp / humidity sensor

--- server/server.ino
+++ server/server.ino
@@ -1,12 +1,18 @@
+#include <SI7021.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <ESP8266WiFiMulti.h>
+#include <Wire.h>
 #include <base64.h>
+#include <WiFiManager.h>
 
 #ifndef STASSID
 #define STASSID "WifiAPName"
 #define STAPSK  "WifiPassword"
 #endif
+
+#define SDA 0 // GPIO0 on ESP-01 module
+#define SCL 2 // GPIO2 on ESP-01 module
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
@@ -14,6 +20,7 @@ const char* password = STAPSK;
 ESP8266WiFiMulti WiFiMulti;
 WiFiUDP udp;
 IPAddress broadcastIp(224, 0, 0, 1);
+SI7021 sensor;
 
 byte inputString[32];
 int i = 0;
@@ -30,6 +37,8 @@ void setup() {
   }
 
   delay(500);
+  Wire.begin(0, 2);
+  sensor.begin(SDA, SCL);
 }
 
 void loop() {
@@ -43,15 +52,22 @@ void loop() {
     }
     if (i == 32) {
       i = 0;
+      int temperature = sensor.getCelsiusHundredths();
+      int humidity = sensor.getHumidityPercent();
+
       String encoded = base64::encode(inputString, 32);
       udp.beginPacketMulticast(broadcastIp, 9000, WiFi.localIP());
-      udp.print("[\"aq\",{\"mac\":\"");
+      udp.print("{\"mac\":\"");
       udp.print(WiFi.macAddress());
       udp.print("\",\"record_id\":");
       udp.print(recordId);
-      udp.print(",\"record\":\"");
+      udp.print(",\"aq\":\"");
       udp.print(encoded);
-      udp.print("\"}]");
+      udp.print("\",\"temperature\":");
+      udp.print(temperature);
+      udp.print(",\"humidity\":");
+      udp.print(humidity);
+      udp.print("}");
       udp.endPacket();
       recordId++;
     }
