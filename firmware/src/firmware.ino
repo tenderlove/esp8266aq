761b251e5e94a98cbff2e987c4f85e3de4b8fec8
Aaron Patterson <tenderlove@ruby-lang.org>
2021-02-12 14:19:42 -0800

    convert to mqtt

--- firmware/src/firmware.ino
+++ firmware/src/firmware.ino
@@ -1,21 +1,19 @@
-#include <SI7021.h>
 #include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-#include <ESP8266WiFiMulti.h>
-#include <Wire.h>
-#include <base64.h>
 #include <WiFiManager.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <PubSubClient.h>
 
-ESP8266WiFiMulti WiFiMulti;
-WiFiUDP udp;
-IPAddress broadcastIp(224, 0, 0, 1);
 Adafruit_BME280 bme;
 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char *mqtt_server = "10.0.1.211";
+const char *mqtt_prefix = "home/test/esp8266aq";
+
 byte inputString[32];
-int i = 0;
-int recordId = 0;
+int inputIdx = 0;
 
 #define LED_PIN 2
 #define G2_PIN 14
@@ -38,60 +36,107 @@ void setup() {
   wifiManager.setAPCallback(configModeCallback);
   wifiManager.autoConnect();
 
-  delay(500);
+  randomSeed(micros());
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 
   if (!bme.begin(0x76)) {
     Serial.println("Couldn't find BME280 sensor");
     delay(50);
   }
-  Serial.swap(); // Switch to sensor
-}
 
-#define WRITE_INFO_TO(output) \
-  output.print("{\"mac\":\""); \
-  output.print(WiFi.macAddress()); \
-  output.print("\",\"version\":1,\"record_id\":"); \
-  output.print(recordId); \
-  output.print(",\"aq\":\""); \
-  output.print(encoded); \
-  output.print("\",\"temperature\":"); \
-  output.print(temperature); \
-  output.print(",\"humidity\":"); \
-  output.print(humidity); \
-  output.print("}");
-
-void printToSerial(int temperature, int humidity, String encoded) {
-  Serial.swap(); // Switch to main serial
-  WRITE_INFO_TO(Serial)
   Serial.flush();
+  client.setServer(mqtt_server, 1883);
   Serial.swap(); // Switch to sensor
 }
 
-void printToUdp(int temperature, int humidity, String encoded) {
-  udp.beginPacketMulticast(broadcastIp, 8000, WiFi.localIP());
-  WRITE_INFO_TO(udp)
-  udp.endPacket();
+void mqtt_reconnect() {
+  static unsigned long last_attempt = 0;
+  if (millis() - last_attempt < 5000) {
+    return;
+  }
+
+  String clientId = "ESP8266AQ-";
+  clientId += String(random(0xffff), HEX);
+  client.connect(clientId.c_str());
+}
+
+#define ARRAY_SIZE(array) ( sizeof( array )/sizeof( array[0] ) )
+
+const char *pms5003_topics[] = {
+  "pm1_0_standard",
+  "pm2_5_standard",
+  "pm10_standard",
+  "pm1_0_env",
+  "pm2_5_env",
+  "concentration_unit",
+  "particles_03um",
+  "particles_05um",
+  "particles_10um",
+  "particles_25um",
+  "particles_50um",
+  "particles_100um"
+};
+
+void mqtt_publish(const char *topic, const char *format, ...) {
+  char full_topic[256];
+  char value[256];
+  sprintf(full_topic, "%s/%s", mqtt_prefix, topic);
+
+  va_list args;
+  va_start(args, format);
+  vsprintf(value, format, args);
+  va_end(args);
+
+  Serial.print(full_topic);
+  Serial.print(" ");
+  Serial.println(value);
+  client.publish(full_topic, value, true);
 }
 
 void loop() {
+  if (!client.connected()) {
+    mqtt_reconnect();
+  }
+
+  if (!client.connected()) {
+    Serial.swap();
+    Serial.println("MQTT client is not connected");
+    Serial.flush();
+    Serial.swap();
+  }
+  client.loop();
+
   while (Serial.available()) {
-    inputString[i] = Serial.read();
-    i++;
-    if (i == 1 && inputString[0] != 0x42) {
-      i = 0;
-    } else if (i == 2 && inputString[1] != 0x4d) {
-      i = 0;
-    } else if (i == 32) {
-      i = 0;
+    inputString[inputIdx] = Serial.read();
+    inputIdx++;
+    if (inputIdx == 1 && inputString[0] != 0x42) {
+      inputIdx = 0;
+    } else if (inputIdx == 2 && inputString[1] != 0x4d) {
+      inputIdx = 0;
+    } else if (inputIdx == 32) {
+      inputIdx = 0;
 
       int temperature = round(bme.readTemperature() * 100);
       int humidity = round(bme.readHumidity() * 100);
 
-      String encoded = base64::encode(inputString, 32);
+      if (client.connected()) {
+        Serial.swap();
+        mqtt_publish("temperature", "%u.%.2u", temperature / 100, temperature % 100);
+        mqtt_publish("humidity", "%u.%.2u", humidity / 100, humidity % 100);
 
-      printToSerial(temperature, humidity, encoded);
-      printToUdp(temperature, humidity, encoded);
-      recordId++;
+        for(unsigned int i = 0; i < ARRAY_SIZE(pms5003_topics); i++) {
+          byte high = inputString[4+i*2];
+          byte low  = inputString[4+i*2+1];
+          unsigned int value = (high << 8) | low;
+
+          mqtt_publish(pms5003_topics[i], "%u", value);
+        }
+        Serial.flush();
+        Serial.swap();
+      }
     }
   }
 }
