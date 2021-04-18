3044511ef872f204ffef9453c4c34fffb8cab595
Aaron Patterson <aaron.patterson@gmail.com>
2021-04-18 21:31:33 +0000

    update mqtt to influx client

--- firmware/utils/mqtt-to-influx.rb
+++ firmware/utils/mqtt-to-influx.rb
@@ -12,39 +12,35 @@ class InfluxWrapper
     @influx = influx
   end
 
-  def write_temp location, value
-    data = {
-      :values => { :value => value, :unit => "C" },
-      :tags   => { :location => location }
-    }
+  def write_co2 location, value, sensor_type
+    data = msg({ :value => value, :unit => "PPM" }, location, sensor_type)
+    @influx.write_point("co2_samples", data)
+  end
 
+  def write_temp location, value, sensor_type
+    data = msg({ :value => value, :unit => "C" }, location, sensor_type)
     @influx.write_point("temperatures", data)
   end
 
-  def write_humidity location, value
-    data = {
-      :values => { :value => value, :unit => "%" },
-      :tags   => { :location => location }
-    }
-
+  def write_humidity location, value, sensor_type
+    data = msg({ :value => value, :unit => "%" }, location, sensor_type)
     @influx.write_point("humidity_samples", data)
   end
 
-  def write_dewpoint location, value
-    data = {
-      :values => { :value => value, :unit => "C" },
-      :tags   => { :location => location }
-    }
-
+  def write_dewpoint location, value, sensor_type
+    data = msg({ :value => value, :unit => "C" }, location, sensor_type)
     @influx.write_point("dewpoint_samples", data)
   end
 
-  def write_pm25 location, sample
-    values = sample.to_h.select { |k, _| k =~ /^(pm|particle|concentration)/ }
-    tags   = { location: location }
-    data   = { :values => values, :tags   => tags }
-
+  def write_pm25 location, value, sensor_type
+    data   = msg({ "pm2_5_standard" => value }, location, sensor_type)
     @influx.write_point("particle_samples", data)
+  end
+
+  private
+
+  def msg values, location, sensor_type
+    { values: values, tags: { location: location, sensor_type: sensor_type } }
   end
 
   def self.connect
@@ -55,19 +51,28 @@ end
 influx = InfluxWrapper.connect
 
 MQTT::Client.connect HOST do |client|
-  client.get "home/+/esp8266aq/+" do |topic, message|
-    if topic =~ /^(.*\/esp8266aq)\/(.*)$/
+  client.get "home/+/esp/+"
+  client.get "home/+/esp8266aq/+"
+  client.get "home/+/+/esp8266aq/+"
+
+  loop do
+    topic, message = client.get
+
+    if topic =~ /^(.*)\/(esp(?:8266aq)?)\/(.*)$/
       location = $1
-      measurement = $2
+      sensor_type = $2 == "esp" ? "fridge" : "room"
+      measurement = $3
       case measurement
       when "humidity"
-        influx.write_humidity location, message.to_f
+        influx.write_humidity location, message.to_f, sensor_type
+      when "co2"
+        influx.write_co2 location, message.to_i, sensor_type
       when "temperature"
-        influx.write_temp location, message.to_f
+        influx.write_temp location, message.to_f, sensor_type
       when "dewpoint"
-        influx.write_dewpoint location, message.to_f
+        influx.write_dewpoint location, message.to_f, sensor_type
       when "pm2_5_standard"
-        influx.write_pm25(location, { "pm2_5_standard" => message.to_i })
+        influx.write_pm25 location, message.to_i, sensor_type
       end
     end
   end
